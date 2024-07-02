3e78b73e47afce3bce959360c123ea0d7b4b3597
Tomasz Łogisz <tomek.logisz@gmail.com>
2024-07-02 16:56:33 +0200

    Create struct calculator

--- hello.go
+++ hello.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+//def structs
+type MyError struct {
+	Code    int
+	Message string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("Error %d: %s", e.Code, e.Message)
+}
+
+func Add(number1 float64, number2 float64) float64 {
+	return number1 + number2
+}
+
+func Multiple(number1 float64, number2 float64) float64 {
+	return number1 * number2
+}
+
+func Subtraction(number1 float64, number2 float64) float64 {
+	return number1 - number2
+}
+
+func Divide(number1 float64, number2 float64) (float64, error) {
+	if number2 == 0 {
+		return 0, &MyError{Code: 400, Message: "Divide by zero"}
+	}
+	return number1 / number2, nil
+}
+
 func main() {
-	fmt.Println("Hello Go!")
+	fmt.Println(Add(3, 5))
+	fmt.Println(Divide(5, 0))
 }
